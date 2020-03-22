f032da16ffd90b12818bfb0709d033470cd0f71a
christianrivero <ch.riverov10@gmail.com>
2020-03-22 00:12:54 -0500

    Add Example: For Loop with filters

---
+++ Capitulo-1-Scala/Page27_2.scala
@@ -0,0 +1,10 @@
+def forListaConFiltros = {
+    val numList = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    for(a <- numList
+        if a<=5
+        if a!=3) {
+            println(a)
+        }
+}
+
+forListaConFiltros
