3c37ab0c903a1968ad35314011a831b3b7f90d27
christianrivero <ch.riverov10@gmail.com>
2020-03-22 01:08:47 -0500

    Add Example: Importing classes from Java

---
+++ Capitulo-1-Scala/Page29.scala
@@ -0,0 +1,9 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat._
+object FrenchDate {
+    def main(args: Array[String]) {
+        val ahora = new Date
+        val df = getDateInstance(LONG, Locale.FRANCE)
+        println(df.format(ahora))
+    }
+}
